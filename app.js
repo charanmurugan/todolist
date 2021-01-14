89550365f57388b501c6d0bb21abbc05cf17402e
SANGEE <murugancharan245@gmail.com>
2021-01-14 16:15:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const express=require('express');
+const app= express();
+const mongoose=require('mongoose');
+const bodyParser=require("body-parser");
+const _=require("lodash");
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+let today="";
+const lists=[];
+let value="";
+mongoose.connect("mongodb+srv://charanmurugan:Charan2002@charanmurugan.axjh5.mongodb.net/todolistdb?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+const todoSchema=new mongoose.Schema({
+    name:String
+});
+const List=mongoose.model("list",todoSchema);
+const item=new List({
+    name:_.capitalize("hello Welcome to My Page")
+});
+
+app.get("/",function(req,res){
+    var date=new Date();
+    const options = { 
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric' };
+   today=date.toLocaleDateString('en', options)
+   List.find({},function(err,foundItems){
+       if(foundItems.length===0){
+         List.insertMany(item,function(err){
+            if(!err){
+                console.log("succesfully");
+            }
+        });
+        res.redirect("/");
+       }
+     else{
+          res.render("index",{
+            kindOfDay:today,
+            lists:foundItems,
+            value:value
+        });
+    }
+   });
+
+});
+app.post("/",function(req,res){
+    value="list_area";
+    const todoValue=_.capitalize(req.body.todo_value);
+    const item1=new List({
+        name:todoValue
+    });
+    item1.save();
+
+
+    res.redirect("/");    
+})
+app.post("/delete",function(req,res){
+    const checkedValue=(req.body.checked);
+    console.log(checkedValue);
+    List.findByIdAndRemove(checkedValue,function(err){
+    if(!err){
+             res.redirect("/"); 
+    }
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(req,res){
+    console.log("server started");
+})
